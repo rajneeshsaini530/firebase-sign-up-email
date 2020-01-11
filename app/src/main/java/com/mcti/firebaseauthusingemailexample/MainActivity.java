3907551070232a5149b2edbe8512e64deb03250a
Rajneesh kumar <36737053+rajneeshsaini530@users.noreply.github.com>
2020-01-11 11:08:53 +0530

    first commit

---
+++ app/src/main/java/com/mcti/firebaseauthusingemailexample/MainActivity.java
@@ -0,0 +1,56 @@
+package com.mcti.firebaseauthusingemailexample;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
+    private EditText email,password;
+    private Button signUp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        firebaseAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        signUp= findViewById(R.id.sign_up);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userEmail = email.getText().toString().trim();
+                String userPass = password.getText().toString().trim();
+                if("".equals(userEmail)){
+                    email.setError("Enter Email");
+                }else if("".equals(userPass)){
+                    password.setError("Enter Password");
+                }
+                else {
+                    firebaseAuth.createUserWithEmailAndPassword(userEmail,userPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+
+                                //do something when sign up success
+                                Toast.makeText(MainActivity.this, "signup sucessfull", Toast.LENGTH_SHORT).show();
+                            }else{
+                                Toast.makeText(MainActivity.this, "sign up failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
